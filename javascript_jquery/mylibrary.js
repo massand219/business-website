552779894db49ebc62ad50f9cee23d2339b0d99e
Mehak Massand <massand219@gmail.com>
2015-10-26 23:05:12 -0400

    fixed some css, added jquery for image slider, added images for fade-in, fade-out for image1,2,3, didn't add jquery for fade in yet.

---
+++ javascript_jquery/mylibrary.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+$(function(){
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+      $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');}, 
+      1500);
+});
+
+
+
+})
+
