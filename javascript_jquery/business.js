1435355014cd359fc6792090e22ae297ae0e782b
Mehak Massand <massand219@gmail.com>
2015-10-29 19:56:33 -0400

    updated index with julie

---
+++ javascript_jquery/business.js
@@ -0,0 +1,49 @@
+'use strict';
+
+$(function() {
+
+ //settings for slider
+    var width = 720;
+    var animationSpeed = 1000;
+    var pause = 3000;
+    var currentSlide = 1;
+
+    //cache DOM elements
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+    startSlider();
+
+
+});
+
+
+
+
+
+	//setInterval(function() {
+	//	$('#slider .slides').animate({'margin-left': '-=720px'}, 1000);
+	//}, 3000);
+
+//});
